208de68093bb7be66b404569b63a82265af26deb
E1ijahJ <elijahguijoseph@gmail.com>
2025-04-10 23:35:54 -0400

    Added header to the code

--- include/dll.h
+++ include/dll.h
@@ -17,6 +17,7 @@ void insert_at_index(int index, int data);
 bool delete_element(int data);
 bool delete_at_index(int index);
 bool update_element(int old_data, int new_data);
+bool update_element_at_index(int index, int data);
 private:
     Node* head;
 };
