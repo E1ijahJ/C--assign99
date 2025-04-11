47834ac604d5ef445df3b1ffafd448e8a554df35
E1ijahJ <elijahguijoseph@gmail.com>
2025-04-10 23:16:38 -0400

    Added testing to ensure everything works

--- tests/LinkedLists_tests.cpp
+++ tests/LinkedLists_tests.cpp
@@ -11,3 +11,14 @@ TEST(DLLTest, InsertAtTailWorks) {
     EXPECT_EQ(list.get(1), 20);
     EXPECT_EQ(list.get(2), 30);
 }
+
+TEST(DLLTest, InsertAtIndexWorks) {
+    DynamicLinkedList list;
+    list.insert(10);
+    list.insert(20);
+    list.insert_at_index(1, 15);
+
+    EXPECT_EQ(list.get(0), 10);
+    EXPECT_EQ(list.get(1), 15);
+    EXPECT_EQ(list.get(2), 20);
+}
