82ba6bec9e24dbe3c47aa48d57b4eed7a0be3e9f
E1ijahJ <elijahguijoseph@gmail.com>
2025-04-10 23:55:18 -0400

    Added insert and get for logic

---
+++ src/sll.cpp
@@ -0,0 +1,41 @@
+#include "sll.h"
+
+SLL::SLL() {
+    for (int i = 0; i < MAX_SIZE - 1; ++i)
+        next[i] = i + 1;
+    next[MAX_SIZE - 1] = -1;
+
+    head = -1;
+    freeIndex = 0;
+    size = 0;
+}
+
+void SLL::insert(int value) {
+    if (freeIndex == -1) return;
+
+    int newNode = freeIndex;
+    freeIndex = next[freeIndex];
+    data[newNode] = value;
+    next[newNode] = -1;
+
+    if (head == -1) {
+        head = newNode;
+    } else {
+        int current = head;
+        while (next[current] != -1)
+            current = next[current];
+        next[current] = newNode;
+    }
+
+    size++;
+}
+
+int SLL::get(int index) const {
+    if (index < 0 || index >= size)
+        return -1;
+
+    int current = head;
+    for (int i = 0; i < index; ++i)
+        current = next[current];
+    return data[current];
+}
