dafca70f0e0c6105ed0c8eab7f3aab958d43c27a
E1ijahJ <elijahguijoseph@gmail.com>
2025-04-10 23:05:31 -0400

    Added code for the list

--- src/dll.cpp
+++ src/dll.cpp
@@ -1,5 +1,42 @@
 #include "dll.h"
+#include <stdexcept>
 
-int add(int a, int b) {
-    return a + b;
+DynamicLinkedList::DynamicLinkedList() : head(nullptr) {}
+
+DynamicLinkedList::~DynamicLinkedList() {
+    Node* current = head;
+    while (current) {
+        Node* next = current->next;
+        delete current;
+        current = next;
+    }
+}
+
+void DynamicLinkedList::insert(int data) {
+    Node* newNode = new Node{data, nullptr};
+
+    if (!head) {
+        head = newNode;
+    } else {
+        Node* current = head;
+        while (current->next) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+}
+
+int DynamicLinkedList::get(int index) const {
+    Node* current = head;
+    int currentIndex = 0;
+
+    while (current) {
+        if (currentIndex == index) {
+            return current->data;
+        }
+        current = current->next;
+        currentIndex++;
+    }
+
+    throw std::out_of_range("Index out of bounds");
 }
