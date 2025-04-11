845f7605fe8e94bd92248937b9a43188c0059e57
E1ijahJ <elijahguijoseph@gmail.com>
2025-04-11 00:48:27 -0400

    Added documentation for the functions

--- src/dll.cpp
+++ src/dll.cpp
@@ -11,7 +11,11 @@ DynamicLinkedList::~DynamicLinkedList() {
         current = next;
     }
 }
-
+/**
+ * @brief Inserts a new node with the specified data at the end of the list.
+ * 
+ * @param data The value to insert.
+ */
 void DynamicLinkedList::insert(int data) {
     Node* newNode = new Node{data, nullptr};
 
@@ -25,7 +29,13 @@ void DynamicLinkedList::insert(int data) {
         current->next = newNode;
     }
 }
-
+/**
+ * @brief Retrieves the value at a specified index.
+ * 
+ * @param index The position of the element to retrieve.
+ * @return The value at the given index.
+ * @throws std::out_of_range if the index is invalid.
+ */
 int DynamicLinkedList::get(int index) const {
     Node* current = head;
     int currentIndex = 0;
@@ -40,6 +50,12 @@ int DynamicLinkedList::get(int index) const {
 
     throw std::out_of_range("Index out of bounds");
 }
+/**
+ * @brief Inserts a new node with the specified data at the given index.
+ * 
+ * @param index The index to insert the new data at.
+ * @param data The value to insert.
+ */
 void DynamicLinkedList::insert_at_index(int index, int data) {
     if (index < 0) return;
 
@@ -66,6 +82,12 @@ void DynamicLinkedList::insert_at_index(int index, int data) {
     newNode->next = current->next;
     current->next = newNode;
 }
+/**
+ * @brief Deletes the first node that contains the specified data.
+ * 
+ * @param data The value to delete.
+ * @return true if a node was deleted, false otherwise.
+ */
 bool DynamicLinkedList::delete_element(int data) {
     if (!head) return false;
 
@@ -89,7 +111,12 @@ bool DynamicLinkedList::delete_element(int data) {
     delete toDelete;
     return true;
 }
-
+/**
+ * @brief Deletes the node at the specified index.
+ * 
+ * @param index The index of the node to delete.
+ * @return true if deletion was successful, false otherwise.
+ */
 bool DynamicLinkedList::delete_at_index(int index) {
     if (index < 0 || !head) return false;
 
@@ -112,6 +139,13 @@ bool DynamicLinkedList::delete_at_index(int index) {
     delete toDelete;
     return true;
 }
+/**
+ * @brief Updates the first node that matches old_data to new_data.
+ * 
+ * @param old_data The value to search for.
+ * @param new_data The value to replace it with.
+ * @return true if an update occurred, false otherwise.
+ */
 bool DynamicLinkedList::update_element(int old_data, int new_data) {
     Node* current = head;
     while (current) {
@@ -123,6 +157,13 @@ bool DynamicLinkedList::update_element(int old_data, int new_data) {
     }
     return false;
 }
+/**
+ * @brief Updates the data at the specified index.
+ * 
+ * @param index The index to update.
+ * @param data The new value to set.
+ * @return true if update was successful, false otherwise.
+ */
 bool DynamicLinkedList::update_element_at_index(int index, int data) {
     if (index < 0) return false;
 
@@ -140,6 +181,12 @@ bool DynamicLinkedList::update_element_at_index(int index, int data) {
 
     return false; // Index out of bounds
 }
+/**
+ * @brief Searches for the given value in the list.
+ * 
+ * @param data The value to search for.
+ * @return true if the value is found, false otherwise.
+ */
 bool DynamicLinkedList::find(int data) {
     Node* current = head;
     while (current) {
@@ -149,6 +196,13 @@ bool DynamicLinkedList::find(int data) {
     }
     return false;
 }
+/**
+ * @brief Gets the value at the specified index (non-const overload).
+ * 
+ * @param index The index to access.
+ * @return The value at the index.
+ * @throws std::out_of_range if the index is invalid.
+ */
 int DynamicLinkedList::get(int index) {
     if (index < 0) return -1; 
     Node* current = head;
