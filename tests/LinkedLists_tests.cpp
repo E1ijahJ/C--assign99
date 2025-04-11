6779792217ae1e6c7a03adc104647fd6249e2de3
E1ijahJ <elijahguijoseph@gmail.com>
2025-04-10 23:19:49 -0400

    implented testing for the function

--- tests/LinkedLists_tests.cpp
+++ tests/LinkedLists_tests.cpp
@@ -22,3 +22,15 @@ TEST(DLLTest, InsertAtIndexWorks) {
     EXPECT_EQ(list.get(1), 15);
     EXPECT_EQ(list.get(2), 20);
 }
+TEST(DLLTest, DeleteElementWorks) {
+    DynamicLinkedList list;
+    list.insert(1);
+    list.insert(2);
+    list.insert(3);
+
+    EXPECT_TRUE(list.delete_element(2));
+    EXPECT_FALSE(list.delete_element(4)); // not in list
+
+    EXPECT_EQ(list.get(0), 1);
+    EXPECT_EQ(list.get(1), 3);
+}
